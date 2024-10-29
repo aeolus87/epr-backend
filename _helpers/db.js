1e6f00242411276401b32599118297f9d96a479a
aeolus87 <callejo.julius.07222003@gmail.com>
2024-10-29 16:56:20 +0800

    first api fix

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,18 +1,27 @@
-//_helpers/db.js
+// _helpers/db.js
 
 const mongoose = require("mongoose");
 
-const uri =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://aeodev:epr-clan522@epr.mzolr.mongodb.net/EPR";
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  console.error(
+    "MongoDB connection URI is not defined. Please set MONGODB_URI in your environment variables."
+  );
+  process.exit(1);
+}
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
     process.exit(1);
   });
 
+// Export your models
 module.exports = {
   Homepage: require("../homepage/homepage.model"),
 };
